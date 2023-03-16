850ed799b1aaa0c1736d6c8e7a16c18d91c720f8
Ming-der Wang <mingderwang@gmail.com>
2023-03-16 18:17:54 +0800

    html, run with deno

---
+++ index.tsx
@@ -0,0 +1,15 @@
+import React from 'https://esm.sh/react'
+import ReactDOMServer from 'https://esm.sh/react-dom/server'
+
+function App() {
+  return (
+    <div>
+      <h1>Hello, Deno!</h1>
+      <p>This is a React app running on Deno.</p>
+    </div>
+  );
+}
+
+const html = ReactDOMServer.renderToString(<App />);
+console.log(html);
+
